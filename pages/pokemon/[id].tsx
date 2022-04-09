f7e5bebc88ff309a522d840dcef5921191ff873a
Jorge <jarancibia@waypoint.com>
2022-04-09 01:23:15 -0400

    Refactor dinamic page

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -6,24 +6,26 @@ import { CompletePokemon } from '../../interfaces';
 import { pokemon } from '../../api';
 import { Button, Container, Grid, Image, Text, Card } from '@nextui-org/react';
 import { colors } from '../../styles/theme';
+import Head from 'next/head';
 
 interface Props {
 	pokemon: CompletePokemon;
 }
 
 const DetailsPage: NextPage<Props> = ({ pokemon }) => {
+	const { name, sprites } = pokemon;
 	return (
-		<Layout title={pokemon.name}>
+		<Layout title={name}>
 			<Grid.Container css={{ marginTop: '5px' }} gap={2}>
 				<Grid xs={12} sm={4}>
 					<Card css={{ padding: '30px' }} hoverable>
 						<Card.Body>
 							<Card.Image
 								src={
-									pokemon.sprites.other?.dream_world.front_default ||
+									sprites.other?.dream_world.front_default ||
 									'/imagen-sin-recurso-disponible.png'
 								}
-								alt={pokemon.name}
+								alt={name}
 								width='100%'
 								height={200}
 							/>
@@ -33,53 +35,23 @@ const DetailsPage: NextPage<Props> = ({ pokemon }) => {
 				<Grid xs={12} sm={8}>
 					<Card css={{ padding: '30px' }} hoverable>
 						<Card.Header
-							css={{ display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap' }}
+							css={{
+								display: 'flex',
+								justifyContent: 'space-between',
+								flexWrap: 'wrap',
+							}}
 						>
 							<Text h1 transform='capitalize'>
 								{pokemon.name}
 							</Text>
-							<Button
-								css={{
-									backgroundColor: colors.darkOrange,
-									color: 'white',
-									'@xsMax': { width: '1em' },
-									'@xs': { width: '1em' },
-									'@sm': { width: '1em' },
-									'@md': { width: '1.1em' },
-									'@lg': { width: '1.3em' },
-								}}
-								ghost
-							>
-								Guardar en favoritos
-							</Button>
 						</Card.Header>
 						<Card.Body>
 							<Text size={30}>Sprites:</Text>
 							<Container direction='row' display='flex' gap={0}>
-								<Image
-									src={pokemon.sprites.front_default}
-									alt={pokemon.name}
-									width={100}
-									height={100}
-								/>
-								<Image
-									src={pokemon.sprites.back_default}
-									alt={pokemon.name}
-									width={100}
-									height={100}
-								/>
-								<Image
-									src={pokemon.sprites.front_shiny}
-									alt={pokemon.name}
-									width={100}
-									height={100}
-								/>
-								<Image
-									src={pokemon.sprites.back_shiny}
-									alt={pokemon.name}
-									width={100}
-									height={100}
-								/>
+								<Image src={sprites.front_default} alt={name} width={100} height={100} />
+								<Image src={sprites.back_default} alt={name} width={100} height={100} />
+								<Image src={sprites.front_shiny} alt={name} width={100} height={100} />
+								<Image src={sprites.back_shiny} alt={name} width={100} height={100} />
 							</Container>
 						</Card.Body>
 					</Card>

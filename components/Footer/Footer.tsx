5aaa9344e2bbfdb185f21fed96249b306e90b988
Jorge <jarancibia@waypoint.com>
2022-04-09 00:14:32 -0400

    footer ok

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,56 +1,63 @@
-import { Text, Link } from '@nextui-org/react';
-import Image from 'next/image';
+import { Text, Link, Container, Row, Image } from '@nextui-org/react';
 import { colors } from '../../styles/theme';
 
 export const Footer = () => {
 	return (
-		<footer className='footer d-flex-center'>
-			<Text
-				css={{
-					color: colors.blackFont,
-					'@xsMax': { fontSize: '0.8em' },
-					'@xs': { fontSize: '1em' },
-					'@sm': { fontSize: '1em' },
-					'@md': { fontSize: '1.1em' },
-					'@lg': { fontSize: '1.3em' },
-				}}
-			>
-				Made by
-				<Link
-					href='https://github.com/JorgeArancibia1'
-					css={{
-						color: colors.darkOrange,
-						fontWeight: 'bold',
-						'@xsMax': { fontSize: '1em' },
-						'@xs': { fontSize: '1em' },
-						'@sm': { fontSize: '1em' },
-						'@md': { fontSize: '1em' },
-						'@lg': { fontSize: '1em' },
-					}}
-				>
-					&nbsp;Jorge Arancibia&nbsp;
-				</Link>
-			</Text>
-			<Text
-				css={{
-					color: colors.blackFont,
-					'@xsMax': { fontSize: '0.8em' },
-					'@xs': { fontSize: '1em' },
-					'@sm': { fontSize: '1em' },
-					'@md': { fontSize: '1.1em' },
-					'@lg': { fontSize: '1.3em' },
-				}}
-			>
-				for Houm ❤️
-			</Text>
-			<span>
-				<Image
-					src='https://houm.com/static/brandImage/houmLogo.svg'
-					alt='Vercel Logo'
-					width={72}
-					height={16}
-				/>
-			</span>
-		</footer>
+		<Container>
+			<Row justify='center' align='center'>
+				<Row>
+					<Text
+						b
+						css={{
+							color: colors.blackFont,
+							'@xsMax': { fontSize: '0.8rem' },
+							'@xs': { fontSize: '0.9rem' },
+							'@sm': { fontSize: '1rem' },
+							'@md': { fontSize: '1.1rem' },
+							'@lg': { fontSize: '1.2rem' },
+						}}
+					>
+						Made by
+					</Text>
+					<Link
+						href='https://github.com/JorgeArancibia1'
+						css={{
+							color: colors.darkOrange,
+							fontWeight: 'bold',
+							'@xsMax': { fontSize: '1rem' },
+							'@xs': { fontSize: '1rem' },
+							'@sm': { fontSize: '1.1rem' },
+							'@md': { fontSize: '1.2rem' },
+							'@lg': { fontSize: '1.2rem' },
+						}}
+					>
+						&nbsp;Jorge Arancibia&nbsp;
+					</Link>
+					<Text
+						b
+						css={{
+							color: colors.blackFont,
+							'@xsMax': { fontSize: '0.8rem' },
+							'@xs': { fontSize: '0.9rem' },
+							'@sm': { fontSize: '1rem' },
+							'@md': { fontSize: '1.1rem' },
+							'@lg': { fontSize: '1.2rem' },
+						}}
+					>
+						for
+					</Text>
+				</Row>
+				<Row>
+					<Image
+						src='https://houm.com/static/brandImage/houmLogo.svg'
+						alt='Vercel Logo'
+						width={82}
+						height={36}
+					/>
+
+					<Text>❤️</Text>
+				</Row>
+			</Row>
+		</Container>
 	);
 };
